13944e7738456b46b308412db98d3981ca96645f
Tomek Wiszniewski <tomek@lofty.studio>
2021-03-11 11:48:22 +0100

    Fix bug

--- generate_large_pngs.fish
+++ generate_large_pngs.fish
@@ -2,6 +2,6 @@
 mkdir -p assets/512x512
 set fish_trace 1
 for file in assets/svg/*
-	convert $file -resize 512x (echo $file | gsed 's/\.svg$/.png/')
+	convert $file -resize 512x assets/512x512/(basename $file .svg).png
 end
 set --erase fish_trace
