9766eb10985249938496c96c83c9d105adf949da
Tomek Wiszniewski <tomek@lofty.studio>
2021-03-11 11:53:38 +0100

    Use Inkscape instead of ImageMagick

--- generate_large_pngs.fish
+++ generate_large_pngs.fish
@@ -2,6 +2,6 @@
 mkdir -p assets/512x512
 set fish_trace 1
 for file in assets/svg/*
-	convert $file -resize 512x assets/512x512/(basename $file .svg).png
+	inkscape -w 512 $file -o assets/512x512/(basename $file .svg).png
 end
 set --erase fish_trace
